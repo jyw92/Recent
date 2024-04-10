af335fb7d9ea03e369d2dcd1cf4caf6ae68276ea
Ji Young Woong <koreajyy@gmail.com>
2024-04-11 01:35:22 +0900

    test

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>test</Text>
+      <Text>멍청하다</Text>
       <StatusBar style="auto" />
     </View>
   );
