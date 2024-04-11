014a2be5d2effe0c574c787eea4b32aac0acdc84
jyw92 <67307767+jyw92@users.noreply.github.com>
2024-04-11 16:46:11 +0900

    2024-04-11
    overriding

--- App.js
+++ App.js
@@ -1,12 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import ManageExpense from './screens/ManageExpense';
+import RecentExpenses from './screens/RecentExpenses';
+import AllExpenses from './screens/AllExpenses';
+
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+const ExpensesOverView = () => {
+  return (
+    <BottomTabs.Navigator screenOptions={
+        {
+          
+        }
+      }
+    >
+      <BottomTabs.Screen 
+        name='RecentExpenses' 
+        component={RecentExpenses} 
+      />
+      <BottomTabs.Screen 
+        name='AllExpenses' 
+        component={AllExpenses} 
+      />
+    </BottomTabs.Navigator>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style='auto'/>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen 
+            name="ManageExpense" 
+            component={ManageExpense}
+            options={
+              {
+                headerShown:false
+              }
+            }
+          />
+          <Stack.Screen 
+            name="ExpensesOverView" 
+            component={ExpensesOverView}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
